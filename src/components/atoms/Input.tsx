4b28e95dba918de07d6e8ebc41a0ae721ab4ecbf
Iman Chamileva <iman.moussaevna@gmail.com>
2022-06-24 13:06:26 +0200

    todo app check square and delete ok

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,9 +1,11 @@
 import React, { ChangeEvent } from 'react'
 import {useState} from 'react'
-
+import { FaCheckSquare } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 type ICardItem = {
 text: string;
+id: number;
   };
 
 export const Input = () => {
@@ -34,16 +36,21 @@ export const Input = () => {
             <form onSubmit={onButtonClick}>
                 <input type="text" 
                 value={getInput} 
-                onChange={handleChange} />
+                onChange={handleChange} 
+                className="border-2 rounded" />
 
-                <button>Add</button>
+                <button className="px-8 py-1 bg-green-600 text-white rounded">Add</button>
             </form>
             <div>
                 <ul>
                         {arrayData.map((elem : ICardItem) => (
-                            <li>
-                                {elem.text}
+                            <li key={elem.id}>
+                              <span className="text-2xl">{elem.text}</span> 
+                              <button className='cursor-pointer bg-green-500 px-2 py-2'><FaCheckSquare className='cursor-pointer' /></button>
+
+                              <button className='cursor-pointer bg-red-500 px-2 py-2'><FaTimes /></button>
                             </li>
+                        
                         ) )}
 
                 </ul>
