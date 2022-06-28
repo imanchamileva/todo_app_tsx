f4a8eaf64e35e229e791f65173af86eec2d0ef5b
Iman Chamileva <iman.moussaevna@gmail.com>
2022-06-28 15:32:36 +0200

    working on delete elem

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,7 +1,10 @@
+import { nanoid } from 'nanoid';
 import React, { ChangeEvent } from 'react'
 import {useState} from 'react'
 import { FaCheckSquare } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
+
+
 
 type ICardItem = {
 text: string;
@@ -22,16 +25,15 @@ export const Input = () => {
 
     const onButtonClick = (e :React.FormEvent<HTMLFormElement> ) :void => {
     e.preventDefault();
-    setArrayData([...arrayData,
-         {text: getInput, completed: false, id :1}]
-         );
-         setGetInput('')
+    let newEntry = {id :nanoid, title: getInput, completed: false}
+    setArrayData([...arrayData, newEntry]);
+    setGetInput('')
     }
 
 
-    const removeItem = () => {
+    const removeItem = (id: number) : void => {
 
-        const newList = arrayData.filter((item:ICardItem) => item.id !== item.id)
+        const newList = arrayData.filter((item: ICardItem) => item.id != id)
         setArrayData(newList);
     }
 
