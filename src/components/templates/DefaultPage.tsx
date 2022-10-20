0d0775e693f3dac0d3a42cb0b11987f2dcc07908
Iman Chamileva <iman.moussaevna@gmail.com>
2022-10-20 11:39:35 +0200

    button disappering after clicking

--- src/components/templates/DefaultPage.tsx
+++ src/components/templates/DefaultPage.tsx
@@ -1,4 +1,3 @@
-import AddNewTask from '../atoms/AddNewTask';
 import { Input } from '../atoms/Input'
 import StyledButton from '../atoms/StyledButton';
 import { Title } from '../atoms/Title';
@@ -7,19 +6,20 @@ import { useState } from 'react'
 export const DefaultPage = () => {
 
     const [showInput, setShowInput] = useState<any>(false);
-
+    const [hideButton, setHideButton] = useState<any>(true)
 
     const showInputHandle = ()  => {
-      
         setShowInput(true)
-        console.log('lal');
-        
+        setHideButton(false)
     }
+
     return (
         <section className="h-screen w-100 bg-gradient-to-r from-green-500 to-yellow-500 flex justify-center items-center">
             <section className="h-2/3 bg-gray-100 rounded-xl w-2/3 flex flex-col items-center justify-around">
             <Title text="My Tasks" />
+            {hideButton &&
                 <StyledButton isSecondary={true} onClick={showInputHandle}><Title text="Add a new task" /></StyledButton>
+            }
                   {showInput && (
                 <Input />
                 )
